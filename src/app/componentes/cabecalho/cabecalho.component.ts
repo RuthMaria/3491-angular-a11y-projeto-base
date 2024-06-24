0a76c19c22289854f82750abe0b052c9d3fb8ca8
RuthMaria <ruth-15rms@hotmail.com>
2024-06-23 21:36:30 -0300

    feat: identificando a rota ativa

--- src/app/componentes/cabecalho/cabecalho.component.ts
+++ src/app/componentes/cabecalho/cabecalho.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterLink, RouterOutlet } from '@angular/router';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-cabecalho',
   standalone: true,
-  imports: [CommonModule, RouterLink, RouterOutlet],
+  imports: [CommonModule, RouterLink, RouterOutlet, RouterLinkActive],
   templateUrl: './cabecalho.component.html',
   styleUrl: './cabecalho.component.css'
 })
